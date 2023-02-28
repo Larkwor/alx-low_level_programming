082680d68d7f5f544b961f3cb19c0d7600dfefc8
Larkwor <rosesarahglover@gmail.com>
2023-02-28 14:30:58 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
  * @s: string
  * Return: length
  */
- int _strlen(char *s)
- {
-      int longi = 0;
+int _strlen(char *s)
+{
+int longi = 0;
 
-      while (*s != '\0')
-      {
+while (*s != '\0')
+{
 	    longi++;
 	    s++;
-      }
- return (longi);
- }
+}
+return (longi);
+}
