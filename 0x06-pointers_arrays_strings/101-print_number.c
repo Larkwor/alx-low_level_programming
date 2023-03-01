034386cb9418e7bac9040d411c381215a5bd0b7b
Larkwor <rosesarahglover@gmail.com>
2023-03-01 11:12:11 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,3 +1,8 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
 
 /**
@@ -10,12 +15,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		num = -num;
 	}
 
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
-	_putchar((num % 10) + '0');
+	putchar((num % 10) + '0');
 }
