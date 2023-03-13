dfcb8fa4b714c3c0483896bd6ec5ee8692e562b6
Larkwor <rosesarahglover@gmail.com>
2023-03-13 12:44:45 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,22 +1,49 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * create_array - creates an array of chars.
- * @size: size of the array.
- * @c: storaged char
- * Return: pointer of an array of chars
- * betty style doc for function main goes there
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ *
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
-char *create_array(unsigned int size, char c)
+char *str_concat(char *s1, char *s2)
 {
-char *al;
-unsigned int i;
-if (size == 0)
-return (NULL);
-al = malloc(sizeof(c) * size);
-if (al == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-al[i] = c;
-return (al);
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
+	}
+
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
 }
