2fcd90c733bea7b95ed9556e0c50e27b379830ec
Larkwor <rosesarahglover@gmail.com>
2023-04-11 15:55:23 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output.
@@ -11,42 +13,41 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+        int fd;
+        ssize_t bytes_read, bytes_written;
+        char *buffer;
 
-	if (filename == NULL)
-		return (0);
+        if (filename == NULL)
+                return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+        buffer = malloc(sizeof(char) * letters);
+        if (buffer == NULL)
+                return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+        fd = open(filename, O_RDONLY);
+        if (fd == -1)
+        {
+                free(buffer);
+                return (0);
+        }
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+        bytes_read = read(fd, buffer, letters);
+        if (bytes_read == -1)
+        {
+                free(buffer);
+                close(fd);
+                return (0);
+        }
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+        bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+        if (bytes_written == -1 || bytes_written != bytes_read)
+        {
+                free(buffer);
+                close(fd);
+                return (0);
+        }
 
-	free(buffer);
-	close(fd);
-
-	return (bytes_written);
+        free(buffer);
+        close(fd);
+        return (bytes_written);
 }
