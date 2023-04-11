4c1c9ea5be5988b1db9b02861dd8acde526f3ca7
Larkwor <rosesarahglover@gmail.com>
2023-04-11 12:18:34 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-/** 
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: A pointer to the name of the file.
- * @text_content: string
- * Return: actual number of letters it can read and print
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The number of letters it could read and print.
+ *         0 if the file can not be opened or read, or if write fails or does not write the expected amount of bytes.
  */
- 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        return 0;
-    }
+	if (filename == NULL)
+		return (0);
 
-    char *buffer = (char *)malloc(letters + 1);
-    if (buffer == NULL) {
-        fclose(file);
-        return 0;
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    ssize_t bytes_read = fread(buffer, sizeof(char), letters, file);
-    if (bytes_read <= 0) {
-        free(buffer);
-        fclose(file);
-        return 0;
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    ssize_t bytes_written = fwrite(buffer, sizeof(char), bytes_read, stdout);
-    free(buffer);
-    fclose(file);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    if (bytes_written != bytes_read) {
-        return 0;
-    }
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    return bytes_written;
+	free(buffer);
+	close(fd);
+
+	return (bytes_written);
 }
