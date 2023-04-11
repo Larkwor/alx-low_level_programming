61f25407dcae6cdcea426d915c66313a63238316
Larkwor <rosesarahglover@gmail.com>
2023-04-11 16:35:12 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,39 @@
-#include "create_file.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 /**
- * create_file - creates a file and writes a string to it
- * @filename: the name of the file to create
- * @text_content: the text to write to the file
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, res, len = 0;
+    int fd, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
+    if (text_content != NULL)
+    {
+        len = strlen(text_content);
+    }
 
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
+    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    if (fd == -1)
+        return (-1);
 
-		res = write(fd, text_content, len);
-		if (res == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+    if (write(fd, text_content, len) != len)
+    {
+        close(fd);
+        return (-1);
+    }
 
-	close(fd);
-
-	return (1);
+    close(fd);
+    return (1);
 }
