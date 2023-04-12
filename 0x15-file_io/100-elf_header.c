4f0e4c01307bf900e10dc708d5fcb29eb513edc9
Larkwor <rosesarahglover@gmail.com>
2023-04-12 04:43:38 -0700

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,56 +4,84 @@
 #include <fcntl.h>
 #include <elf.h>
 
-/**
- * print_elf_header - prints the ELF header information of a file
- * @elf_fd: file descriptor of the ELF file
- */
-void print_elf_header(int elf_fd)
-{
-    Elf64_Ehdr header;
-    ssize_t bytes_read;
-
-    /* Read ELF header */
-    bytes_read = read(elf_fd, &header, sizeof(header));
-    if (bytes_read != sizeof(header) || header.e_ident[EI_MAG0] != ELFMAG0
-        || header.e_ident[EI_MAG1] != ELFMAG1
-        || header.e_ident[EI_MAG2] != ELFMAG2
-        || header.e_ident[EI_MAG3] != ELFMAG3)
-    {
-        fprintf(stderr, "Error: Not an ELF file\n");
-        exit(98);
-    }
-
-    /* Print ELF header information */
-    printf("ELF Header:\n");
+void print_magic(unsigned char *e_ident) {
     printf("  Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++)
-        printf("%02x ", header.e_ident[i]);
+    for (int i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", e_ident[i]);
+    }
     printf("\n");
-    printf("  Class:                             %s\n",
-           header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
-           header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
-           header.e_ident[EI_CLASS] == ELFCLASSNONE ? "none" : "unknown");
-    printf("  Data:                              %s\n",
-           header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-           header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "unknown");
-    printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
-    printf("  OS/ABI:                            %s\n",
-           header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
-           header.e_ident[EI_OSABI] == ELFOSABI_HPUX ? "UNIX - HP-UX" :
-           header.e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
-           header.e_ident[EI_OSABI] == ELFOSABI_LINUX ? "UNIX - Linux" :
-           header.e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
-           header.e_ident[EI_OSABI] == ELFOSABI_IRIX ? "UNIX - IRIX" :
-           header.e_ident[EI_OSABI] == ELFOSABI_FREEBSD ? "UNIX - FreeBSD" :
-           header.e_ident[EI_OSABI] == ELFOSABI_TRU64 ? "UNIX - TRU64" :
-           header.e_ident[EI_OSABI] == ELFOSABI_ARM ? "ARM" :
-           header.e_ident[EI_OSABI] == ELFOSABI_STANDALONE ? "Standalone App" : "UNIX - unknown");
-    printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
-    printf("  Type:                              %s\n",
-           header.e_type == ET_NONE ? "NONE (None)" :
-           header.e_type == ET_REL ? "REL (Relocatable file)" :
-           header.e_type == ET_EXEC ? "EXEC (Executable file)" :
-           header.e_type == ET_DYN ? "DYN (Shared object file)" :
-           header.e_type == ET_CORE ? "CORE (
+}
+
+void print_class(unsigned char e_ident[EI_NIDENT]) {
+    printf("  Class:                             ");
+    switch (e_ident[EI_CLASS]) {
+        case ELFCLASSNONE:
+            printf("none\n");
+            break;
+        case ELFCLASS32:
+            printf("ELF32\n");
+            break;
+        case ELFCLASS64:
+            printf("ELF64\n");
+            break;
+        default:
+            printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+            break;
+    }
+}
+
+void print_data(unsigned char e_ident[EI_NIDENT]) {
+    printf("  Data:                              ");
+    switch (e_ident[EI_DATA]) {
+        case ELFDATANONE:
+            printf("none\n");
+            break;
+        case ELFDATA2LSB:
+            printf("2's complement, little endian\n");
+            break;
+        case ELFDATA2MSB:
+            printf("2's complement, big endian\n");
+            break;
+        default:
+            printf("<unknown: %x>\n", e_ident[EI_DATA]);
+            break;
+    }
+}
+
+void print_version(unsigned char e_ident[EI_NIDENT]) {
+    printf("  Version:                           %d", e_ident[EI_VERSION]);
+    if (e_ident[EI_VERSION] == EV_CURRENT) {
+        printf(" (current)\n");
+    } else {
+        printf("\n");
+    }
+}
+
+void print_osabi(unsigned char e_ident[EI_NIDENT]) {
+    printf("  OS/ABI:                            ");
+    switch (e_ident[EI_OSABI]) {
+        case ELFOSABI_SYSV:
+            printf("UNIX - System V\n");
+            break;
+        case ELFOSABI_HPUX:
+            printf("UNIX - HP-UX\n");
+            break;
+        case ELFOSABI_NETBSD:
+            printf("UNIX - NetBSD\n");
+            break;
+        case ELFOSABI_LINUX:
+            printf("UNIX - Linux\n");
+            break;
+        case ELFOSABI_SOLARIS:
+            printf("UNIX - Solaris\n");
+            break;
+        case ELFOSABI_IRIX:
+            printf("UNIX - IRIX\n");
+            break;
+        case ELFOSABI_FREEBSD:
+            printf("UNIX - FreeBSD\n");
+            break;
+        case ELFOSABI_TRU64:
+            printf("UNIX - TRU64\n");
+            break;
 }
