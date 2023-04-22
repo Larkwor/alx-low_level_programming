71126064eaf22b0ba90ae12ebd92d281d76df17f
Larkwor <rosesarahglover@gmail.com>
2023-04-22 11:24:44 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,18 +9,18 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n == 0)
-    {
-        putchar('0');
-        return;
-    }
+if (n == 0)
+{
+putchar('0');
+return;
+}
 
-    if (n == 1)
-    {
-        putchar('1');
-        return;
-    }
+if (n == 1)
+{
+putchar('1');
+return;
+}
 
-    print_binary(n >> 1);
-    putchar((n & 1) + '0');
+print_binary(n >> 1);
+putchar((n & 1) + '0');
 }
